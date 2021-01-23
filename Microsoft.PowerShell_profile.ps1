dc769386c492ddcb8b24326ab2209d555c83a69d
Mark Kerry <mark-kerry@outlook.com>
2021-01-23 16:17:41 +0000

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Write-Host "Importing posh-git module..." -BackgroundColor DarkGray -ForegroundColor Yellow
+Import-Module posh-git
+Write-Host "Importing oh-my-posh module..." -BackgroundColor DarkGray -ForegroundColor Yellow
+Import-Module oh-my-posh
+Write-Host "Setting theme to Paradox..." -BackgroundColor DarkGray -ForegroundColor Yellow
+Set-Theme Paradox
+Clear-Host
